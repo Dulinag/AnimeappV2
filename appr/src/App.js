880c262aa45e11835151a4728a882daff7406bf5
Dulina <dulinagunasekara@gmail.com>
2023-05-15 16:27:00 -0400

    push89

---
+++ appr/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Titlecontainer = styled.div`
+
+font-size: 50px;
+text-align:center;
+
+`
+
+function App() {
+  return (
+<>
+
+
+
+<Titlecontainer>ANIMANIA</Titlecontainer>
+
+
+
+
+</>
+  );
+}
+
+export default App;
