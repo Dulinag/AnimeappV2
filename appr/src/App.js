dbb6a59f7071f01e83cba34df6707a1ab9fae36f
Dulina <dulinagunasekara@gmail.com>
2023-06-05 14:04:00 -0400

    484uu4

--- appr/src/App.js
+++ appr/src/App.js
@@ -34,11 +34,10 @@ padding-bottom: 3%;
 const Renderresult = styled.div`
 
 display: flex;
-
+text-allign: center;
 flex-direction: row;
 justify-content: space-evenly;
-width: 100%;
-height:100vh;
+
 flex-wrap: wrap;
 overflow-y: scroll;
 
@@ -55,7 +54,7 @@ const PageStyler = {
 };
 
 const App = () => {
-  const [search, setSearch] = useState('Enter AnimeManic');
+  const [search, setSearch] = useState('');
   const [animeData, setAnimeData] = useState();
   const [animeInfo, setAnimeInfo] = useState();
   const [myAnimeList, setMyAnimeList] = useState([]);
@@ -76,8 +75,22 @@ const App = () => {
   const getData = async () => {
     const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`);
     const resData = await res.json();
-    setAnimeData(resData.data);
+  
+    if (resData && resData.data && Array.isArray(resData.data)) {
+      const filteredData = resData.data.filter((anime) => {
+        const genres = anime.genres.map((genre) => genre.name.toLowerCase());
+        return !genres.includes("hentai");
+      });
+  
+      setAnimeData(filteredData);
+    } else {
+      // Handle the case when data is not available or not in the expected format
+      // For example, you could set an error state or display a message to the user
+      console.log("Error: Unable to fetch or filter anime data.");
+    }
   };
+  
+  
 
   useEffect(() => {
     getData();
@@ -89,7 +102,9 @@ const App = () => {
 
       <Centerpage>
         <Centerpage2>
+
           <input
+
             type="text"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
@@ -101,7 +116,9 @@ const App = () => {
           <Renderresult>
             {animeData.map((anime) => (
               <div key={anime.mal_id} className="image-container">
+
                 <img src={anime.images.jpg.image_url} alt={anime.title} />
+
                 <p>{anime.title}</p>
               </div>
             ))}
